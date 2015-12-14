f36600d2df0a6c2708bfac3d20a5be475d111dd4
chase <chase@bryx.com>
2015-12-14 18:09:57 -0500

    Added a Parse Exception. Work on parsing

---
+++ src/main/kotlin/edu/csh/chase/jsonlang/engine/exceptions/ParseException.kt
@@ -0,0 +1,3 @@
+package edu.csh.chase.jsonlang.engine.exceptions
+
+class ParseException(message: String) : Exception(message)
--- src/main/kotlin/edu/csh/chase/jsonlang/engine/parsing/Parser.kt
+++ src/main/kotlin/edu/csh/chase/jsonlang/engine/parsing/Parser.kt
@@ -1,9 +1,7 @@
 package edu.csh.chase.jsonlang.engine.parsing
 
-import edu.csh.chase.jsonlang.engine.models.Parameter
-import edu.csh.chase.jsonlang.engine.models.Program
-import edu.csh.chase.jsonlang.engine.models.Type
-import edu.csh.chase.jsonlang.engine.models.Value
+import edu.csh.chase.jsonlang.engine.models.*
+import edu.csh.chase.jsonlang.engine.models.Function
 import edu.csh.chase.kjson.JsonArray
 import edu.csh.chase.kjson.JsonObject
 
@@ -19,9 +17,26 @@ object Parser {
 
     }
 
+    fun parseFunction(obj: JsonObject): Function {
+        val name = obj.getString("name")
+    }
+
+    fun parseAction(obj: JsonObject): Action? {
+        val name = obj.getString("name") ?: return null
+        val arr = obj.traverseMulti("p", "params", "parameters") as? JsonArray ?: return null
+        val params = arr.map {
+            if ( it !is JsonObject) {
+                return null
+            }
+            parseParameter(it) ?: return null
+        }.toArrayList()
+        return Action(name, params)
+    }
+
     fun parseParameter(obj: JsonObject): Parameter? {
         val name = obj.getString("name") ?: return null
-        val value = obj["value"]
+        val value = parseValue(obj["value"]) ?: return null
+        return Parameter(name, value)
     }
 
     fun parseValue(value: Any?): Value? {
@@ -31,7 +46,7 @@ object Parser {
             is Boolean -> Value(value, Type.Boolean)
             is JsonObject -> Value(value, Type.Object)
             is JsonArray -> Value(value, Type.Array)
-            null -> Value(value, Type.Any)
+            null -> Value(value, Type.MAny)
             else -> null
         }
     }
