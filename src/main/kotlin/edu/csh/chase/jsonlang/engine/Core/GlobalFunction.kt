aed46fa6c9c3815e10b033338ce3f544f25846c8
chase <chase@bryx.com>
2015-12-16 16:53:49 -0500

    Added a function to store global values in the memSpace Added better naming for types

---
+++ src/main/kotlin/edu/csh/chase/jsonlang/engine/Core/GlobalFunction.kt
@@ -0,0 +1,20 @@
+package edu.csh.chase.jsonlang.engine.Core
+
+import edu.csh.chase.jsonlang.engine.Engine
+import edu.csh.chase.jsonlang.engine.NativeFunction
+import edu.csh.chase.jsonlang.engine.models.ParameterDefinition
+import edu.csh.chase.jsonlang.engine.models.Type
+import edu.csh.chase.jsonlang.engine.models.Value
+
+class GlobalFunction(e: Engine) : NativeFunction(e, "setGlobal", null) {
+
+    override val params: List<ParameterDefinition> = listOf(ParameterDefinition("name", Type.String),
+            ParameterDefinition("value", Type.MAny))
+
+    override fun execute(parent: String, vararg params: Value): Any? {
+        engine.mem[params[0].value as String] = params[1]
+        return null
+    }
+
+
+}
--- src/main/kotlin/edu/csh/chase/jsonlang/engine/models/Type.kt
+++ src/main/kotlin/edu/csh/chase/jsonlang/engine/models/Type.kt
@@ -1,7 +1,9 @@
 package edu.csh.chase.jsonlang.engine.models
 
-enum class Type {
-    String, Number, Boolean, Object, Array, Any,
-    MString, MNumber, MBoolean, MObject, MArray, MAny
+enum class Type(val value:String) {
+    String("String"), Number("Number"), Boolean("Boolean"), Object("Object"), Array("Array"), Any("Any"),
+    MString("?String"), MNumber("?Number"), MBoolean("?Boolean"), MObject("?Object"), MArray("?Array"), MAny("?Any")
+
+    override fun toString():kotlin.String = value
 
 }
