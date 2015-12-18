ce0de315f88ceece4fa45356f6b35b2c82f331a7
chase <chase@bryx.com>
2015-12-18 13:30:55 -0500

    Added an If function. Takes three args, a Boolean condition, Any then, and ?Any else

--- src/main/kotlin/edu/csh/chase/jsonlang/engine/Core/IfFunction.kt
+++ src/main/kotlin/edu/csh/chase/jsonlang/engine/Core/IfFunction.kt
@@ -5,12 +5,14 @@ import edu.csh.chase.jsonlang.engine.NativeFunction
 import edu.csh.chase.jsonlang.engine.ParamMap
 import edu.csh.chase.jsonlang.engine.models.ParameterDefinition
 import edu.csh.chase.jsonlang.engine.models.Type
+import edu.csh.chase.jsonlang.engine.models.Value
 import edu.csh.chase.jsonlang.engine.pd
 
 class IfFunction(e: Engine) : NativeFunction(e, "if", Type.MAny) {
-    override val parameters: List<ParameterDefinition> = listOf(pd("condition", Type.Boolean), pd("then", Type.MAny), pd("else", Type.MAny))
+    override val parameters: List<ParameterDefinition> = listOf(pd("condition", Type.Boolean), pd("then", Type.Any), pd("else", Type.MAny))
 
-    override fun execute(parent: String, params: ParamMap): Any? {
-        return null
+    override fun execute(parent: String, params: ParamMap): Value? {
+        return if (params.getBoolean("condition")) params.getValue("then") else params.getValue("else")
+
     }
 }
